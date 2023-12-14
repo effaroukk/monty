95fcdaa80ea4e0edc108316bd3254bca6d08c2fb
effaroukk <efqay1@gmail.com>
2023-12-14 22:49:24 +0000

    mont1 bug

--- monty.h
+++ monty.h
@@ -42,21 +42,35 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-int main(int argc, char *argv[]);
+stack_t *create_node(int n);
 void free_nodes(void);
-stack_t *new_node(int n);
-void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln);
-void mont_print_stack(stack_t **mont_stack, unsigned int mont_line_number);
-void monty_pop_top(stack_t **monty_stack, unsigned int monty_line_number);
-void monty_print_top(stack_t **monty_stack, unsigned int monty_line_number);
+void print_stack(stack_t **, unsigned int);
+void add_to_stack(stack_t **, unsigned int);
+void call_fun(op_func, char *, char *, int, int);
 
-
-
-
-
-
-
-
+void print_top(stack_t **, unsigned int);
+void pop_top(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void add_to_queue(stack_t **, unsigned int);
+void read_file(FILE *);
+int len_chars(FILE *);
+void find_func(char *, char *, int, int);
+void open_file(char *file_name);
+int parse_line(char *buffer, int line_number, int format);
+extern stack_t *head;
+typedef void (*op_func)(stack_t **, unsigned int);
+void err(int error_code, ...);
+void more_err(int error_code, ...);
+void string_err(int error_code, ...);
+void rotr(stack_t **, unsigned int);
+void div_nodes(stack_t **, unsigned int);
+void mul_nodes(stack_t **, unsigned int);
+void mod_nodes(stack_t **, unsigned int);
+void print_char(stack_t **, unsigned int);
+void print_str(stack_t **, unsigned int);
+void rotl(stack_t **, unsigned int);
+void add_nodes(stack_t **, unsigned int);
+void sub_nodes(stack_t **, unsigned int);
 
 
 
